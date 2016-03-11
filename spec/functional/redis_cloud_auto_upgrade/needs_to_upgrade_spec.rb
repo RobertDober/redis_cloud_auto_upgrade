33314f151ba08b9b2efc10fef51ac1789329b905
RobertDober <robert.dober@gmail.com>
2016-03-11 11:16:52 +0100

    needs_to_upgrade? spec implemented

--- spec/functional/redis_cloud_auto_upgrade/needs_to_upgrade_spec.rb
+++ spec/functional/redis_cloud_auto_upgrade/needs_to_upgrade_spec.rb
@@ -14,10 +14,63 @@ RSpec.describe RedisCloudAutoUpgrade, type: :functional  do
   end
 
   context 'default tresshold (50%)' do
-    context 'is not reached' do
-      let(:current_redis_mem_usage) { 14_999_999 }
+    context 'small plan' do
       let(:current_redis_cloud_plan) { 'rediscloud:30' }
-      it { expect(rcau.needs_to_upgrade?).to be_falsey }
-    end # context 'is not reached'
+      context 'is not reached' do
+        let(:current_redis_mem_usage) { 14_999_999 }
+        it { expect(rcau.needs_to_upgrade?).to be_falsey }
+      end # context 'is not reached'
+
+      context 'is reached' do
+        let(:current_redis_mem_usage) { 15_000_000 }
+        it { expect(rcau.needs_to_upgrade?).to be_truthy }
+      end # context 'is reached'
+    end # context 'small plan'
+
+    context 'medium plan' do
+      let(:current_redis_cloud_plan) { 'rediscloud:500' }
+
+      context 'is not reached' do
+        let(:current_redis_mem_usage) { 249_999_999 }
+        it { expect(rcau.needs_to_upgrade?).to be_falsey }
+      end # context 'is not reached'
+
+      context 'is reached' do
+        let(:current_redis_mem_usage) { 250_000_000 }
+        it { expect(rcau.needs_to_upgrade?).to be_truthy }
+      end # context 'is reached'
+    end # context 'medium plan'
+
+    context 'huge plan' do
+      let(:current_redis_cloud_plan) { 'rediscloud:50000' }
+      context 'is not reached' do
+        let(:current_redis_mem_usage) { 24_999_999_999 }
+        it { expect(rcau.needs_to_upgrade?).to be_falsey }
+      end # context 'is not reached'
+
+      context 'is reached' do
+        let(:current_redis_mem_usage) { 25_000_000_000 }
+        it { expect(rcau.needs_to_upgrade?).to be_truthy }
+      end # context 'is reached'
+    end # context 'huge plan'
   end # context 'default tresshold (50%)'
+
+  context 'configured tresshold (80%)' do
+    before do
+      rcau.configure(treshhold: 0.8)
+    end
+    context 'big plan' do
+      let(:current_redis_cloud_plan) { 'rediscloud:1000' }
+
+      context 'is not reached' do
+        let(:current_redis_mem_usage) { 799_999_999 }
+        it { expect(rcau.needs_to_upgrade?).to be_falsey }
+      end # context 'is not reached'
+
+      context 'is reached' do
+        let(:current_redis_mem_usage) { 800_000_000 }
+        it { expect(rcau.needs_to_upgrade?).to be_truthy }
+      end # context 'is reached'
+    end # context 'big plan'
+  end # context 'configured tresshold (80%)'
 end
